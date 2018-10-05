7324851947ca8faf228d9b7117491599eb60521e
IsaSam <isaacsamuel27@gmail.com>
2018-10-05 15:38:43 -0400

    Alert Error Description during SignUp and SignIn

--- ChatApp/ViewControllers/LoginViewController.swift
+++ ChatApp/ViewControllers/LoginViewController.swift
@@ -36,16 +36,36 @@ class LoginViewController: UIViewController {
         PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
             
             if username.isEmpty{
-                print("Please Enter your Username!")
-            } else if password.isEmpty{
-                print("Please Enter your Password!")
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                let alertController = UIAlertController(title: "Username Failed!", message: "Please Enter your Username.", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+            }
+            else if password.isEmpty{
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                let alertController = UIAlertController(title: "Password Failed!", message: "Please Enter your Password.", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
             }
             else{
-            
-                if let error = error {
+                if let error = error{
+                    let errorAlertController = UIAlertController(title: "User Log in Failed!", message: "Plese, Verify your username and password or Check your internet Connection", preferredStyle: .alert)
+                    let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
+                    errorAlertController.addAction(cancelAction)
+                    self.present(errorAlertController, animated: true)
+                    print(error.localizedDescription)
+ 
+                }
+                /*if let error = error {
                     print("User log in failed: \(error.localizedDescription)")
-                } else {
-                    print("User logged in successfully")
+                } */else {
+                    let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                    let alertController = UIAlertController(title: "Logging Success!", message: "Welcome to the Chat.", preferredStyle: .alert)
+                    self.present(alertController, animated: true, completion: nil)
+                    // add the OK action to the alert controller
+                    alertController.addAction(OKAction)
                     // display view controller that needs to shown after successful login
                 }
             }
@@ -63,10 +83,37 @@ class LoginViewController: UIViewController {
         
         // call sign up function on the object
         newUser.signUpInBackground { (success: Bool, error: Error?) in
-            if let error = error {
+            
+            if (newUser.username?.isEmpty)!{
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                let alertController = UIAlertController(title: "Username is Required!", message: "Please Choose your Username.", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+            }
+            else if (newUser.password?.isEmpty)!{
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                let alertController = UIAlertController(title: "Password is Required!", message: "Please Enter a New Password.", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+            }
+            
+            if let error = error{
+                let errorAlertController = UIAlertController(title: "Sign up Failed!!!", message: "Account Already Exist for this username or Check your internet Connection", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "Retry", style: .cancel)
+                errorAlertController.addAction(cancelAction)
+                self.present(errorAlertController, animated: true)
                 print(error.localizedDescription)
-            } else {
-                print("New User Registered successfully")
+            }
+            /*if let error = error {
+                print(error.localizedDescription)
+            } */else {
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action) in}
+                let alertController = UIAlertController(title: "Registered Success!", message: "Now Enter your Username and password to Login.", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
                 // manually segue to logged in view
             }
         }
