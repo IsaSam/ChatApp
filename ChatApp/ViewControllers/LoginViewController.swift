7664e72b342b58ba4b9e22ebe5499a8ab0df25a6
IsaSam <isaacsamuel27@gmail.com>
2018-10-05 10:38:09 -0400

    Login outlets created

--- ChatApp/ViewControllers/LoginViewController.swift
+++ ChatApp/ViewControllers/LoginViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var onLoginButton: UIButton!
+    @IBOutlet weak var onSignUpButton: UIButton!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
