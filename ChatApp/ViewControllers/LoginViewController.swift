c9be9ed113331fa407a096f291ca189fe3ec1bbc
IsaSam <isaacsamuel27@gmail.com>
2018-10-05 11:18:37 -0400

    segue login button

--- ChatApp/ViewControllers/LoginViewController.swift
+++ ChatApp/ViewControllers/LoginViewController.swift
@@ -19,16 +19,17 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBOutlet weak var onLoginButton: UIButton!
-    @IBOutlet weak var onSignUpButton: UIButton!
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func onLoginButton(_ sender: Any) {
+    }
+    @IBAction func onSignUpButton(_ sender: Any) {
+    }
+    
 
 
 }
